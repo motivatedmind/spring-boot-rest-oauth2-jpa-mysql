0e51753263adb44143801c3bdadef4a5d28c4af7
motivatedmind <motivatedmind19@gmail.com>
2023-03-04 19:23:14 +0530

    push uncommited changes due to laptop back up

--- spring-boot-secure-rest/src/main/java/com/ahasan/rest/ApplicationRunner.java
+++ spring-boot-secure-rest/src/main/java/com/ahasan/rest/ApplicationRunner.java
@@ -1,7 +1,10 @@
 package com.ahasan.rest;
 
+import java.sql.SQLException;
+import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 /**
@@ -16,6 +19,11 @@ public class ApplicationRunner {
 	public static void main(String[] args) {
 		SpringApplication.run(ApplicationRunner.class, args);
 		System.out.println("Spring Boot secure rest running.......");
+	}
+
+	@Bean(initMethod = "start", destroyMethod = "stop")
+	public Server h2Server() throws SQLException {
+		return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092");
 	}
 
 }
